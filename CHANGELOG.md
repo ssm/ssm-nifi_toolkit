3514429d052fae1667979baf0dae2230b58dec96
Stig Sandbeck Mathisen <ssm@fnord.no>
2020-01-21 08:33:36 +0100

    Initial version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,10 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
-## Release 0.1.0
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-**Features**
-
-**Bugfixes**
-
-**Known Issues**
+## [0.1.0] - 2020-01-21
+### Added
+- Initial release.
+- Download, install and start Apache NiFi Toolkit.
---
+++ manifests/init.pp
@@ -0,0 +1,67 @@
+# @summary Install Apache NiFi Toolkit
+#
+# Install Apache NiFi Toolkit
+#
+# @param version
+#   The version of Apache NiFi Toolkit. This must match the version in
+#   the tarball.
+#
+# @param download_url
+#   Where to download the binary installation tarball from.
+#
+# @param download_checksum
+#   The expected checksum of the downloaded tarball. This is used for
+#   verifying the integrity of the downloaded tarball.
+#
+# @param download_checksum_type
+#   The checksum type of the downloaded tarball. This is used for
+#   verifying the integrity of the downloaded tarball.
+#
+# @param install_root
+#   The root directory of the nifi toolkit installation.
+#
+# @example Defaults
+#   include nifi_toolkit
+#
+# @example Downloading from a different repository
+#   class { 'nifi_toolkit':
+#     download_url => 'https://repo.local/nifi/nifi-toolkit-1.10.0.tar.gz',
+#   }
+#
+class nifi_toolkit (
+  String $version = '1.10.0',
+  String $download_url = 'http://mirrors.ibiblio.org/apache/nifi/1.10.0/nifi-toolkit-1.10.0-bin.tar.gz',
+  String $download_checksum = 'cdea77679a6b6198a60f5b89d6d97783398b48b8d02b513b081f2456f8949ad5',
+  String $download_checksum_type = 'sha256',
+  Stdlib::Absolutepath $download_tmp_dir = '/var/tmp',
+  Stdlib::Absolutepath $install_root = '/opt/nifi-toolkit',
+) {
+
+  $local_tarball = "${download_tmp_dir}/nifi-toolkit-${version}.tar.gz"
+  $software_directory = "${install_root}/nifi-toolkit-${version}"
+  $cli = "${software_directory}/bin/cli.sh"
+
+  archive { $local_tarball:
+    source        => $download_url,
+    checksum      => $download_checksum,
+    checksum_type => $download_checksum_type,
+    extract       => true,
+    extract_path  => $install_root,
+    creates       => $software_directory,
+    cleanup       => true,
+  }
+
+  file { $install_root:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  exec { "permissions for ${software_directory}":
+    path        => ['/usr/bin'],
+    command     => "chown -Rh root: ${software_directory}",
+    refreshonly => true,
+    subscribe   => Archive[$local_tarball],
+  }
+}
